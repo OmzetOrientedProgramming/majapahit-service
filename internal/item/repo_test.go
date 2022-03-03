8b37a5cc945b8b40dd26b60e7e9b6d4a47164f97
gerrad.natanael <gerrad.natanael@ui.ac.id>
2022-03-03 17:36:50 +0700

    [RED] add test for get list item by name

--- internal/item/repo_test.go
+++ internal/item/repo_test.go
@@ -113,6 +113,59 @@ func TestRepo_GetListItemEmpty(t *testing.T) {
 	
 }
 
+func TestRepo_GetListItemByName(t *testing.T) {
+	listItemExpected := &ListItem{
+		Items: []Item {
+			{
+				ID:         	1,
+				Name:        	"test",
+				Image: 			"test",
+				Price:			10000,
+				Description: 	"test",
+			},
+			{
+				ID:          	2,
+				Name:        	"test",
+				Image: 			"test",
+				Price:			10000,
+				Description: 	"test",
+			},
+		},
+	}
+
+	// Mock DB
+	mockDB, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer mockDB.Close()
+	sqlxDB := sqlx.NewDb(mockDB, "sqlmock")
+
+	// Expectation
+	repoMock := NewRepo(sqlxDB)
+	rows := mock.
+		NewRows([]string{"id", "name", "image", "price", "description"}).
+		AddRow(listItemExpected.Items[0].ID,
+			listItemExpected.Items[0].Name,
+			listItemExpected.Items[0].Image,
+			listItemExpected.Items[0].Price,
+			listItemExpected.Items[0].Description).
+		AddRow(listItemExpected.Items[1].ID,
+			listItemExpected.Items[1].Name,
+			listItemExpected.Items[1].Image,
+			listItemExpected.Items[1].Price,
+			listItemExpected.Items[1].Description)
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT id, name, image, price, description FROM items WHERE name LIKE '%$1%' AND place_id = $2")).
+		WithArgs("test",1).
+		WillReturnRows(rows)
+
+	// Test
+	listItemResult, err := repoMock.GetListItem(1, "test")
+	assert.Equal(t, listItemExpected, listItemResult)
+	assert.NotNil(t, listItemResult)
+	assert.NoError(t, err)
+}
+
 func TestRepo_GetItemByID(t *testing.T) {
 	itemExpected := &Item {
 		ID: 1,
