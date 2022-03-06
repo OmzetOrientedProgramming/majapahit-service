f6ce3f8a6d3c39afb0a4ed237d35d561ea498cce
gerrad.natanael <gerrad.natanael@ui.ac.id>
2022-03-06 18:50:36 +0700

    [GREEN] add implementation of handler to return success response

--- internal/item/handler.go
+++ internal/item/handler.go
@@ -1,5 +1,13 @@
 package item
 
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+	"gitlab.cs.ui.ac.id/ppl-fasilkom-ui/2022/Kelas-B/OOP/majapahit-service/util"
+)
+
 type Handler struct {
 	service Service
 }
@@ -8,4 +16,21 @@ func NewHandler(service Service) *Handler {
 	return &Handler{
 		service: service,
 	}
+}
+
+func (h *Handler) GetListItem(c echo.Context) error {
+	placeIDString := c.Param("placeID")
+	name := c.QueryParam("name")
+
+	placeID, _ := strconv.Atoi(placeIDString)
+
+	listItem, _ := h.service.GetListItem(placeID, name)
+
+	return c.JSON(http.StatusOK, util.APIResponse{
+		Status:  200,
+		Message: "success",
+		Data: map[string]interface{}{
+			"places":     listItem.Items,
+		},
+	})
 }
