f95c9a060f1480dbe3ea1d991f3f4c54aea5e730
gerrad.natanael <gerrad.natanael@ui.ac.id>
2022-03-07 12:16:52 +0700

    [RED] add test for internal server error

--- internal/item/handler_test.go
+++ internal/item/handler_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 
 	"github.com/labstack/echo/v4"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"gitlab.cs.ui.ac.id/ppl-fasilkom-ui/2022/Kelas-B/OOP/majapahit-service/util"
@@ -125,5 +126,45 @@ func TestHandler_GetListItemPlaceIDError(t *testing.T) {
 	// Tes
 	assert.NoError(t, h.GetListItem(ctx))
 	assert.Equal(t, http.StatusBadRequest, rec.Code)
+	assert.Equal(t, string(expectedResponseJson), strings.TrimSuffix(rec.Body.String(), "\n"))
+}
+
+func TestHandler_GetListItemInternalServerError(t *testing.T) {
+	// Setup echo
+	e := echo.New()
+
+	// import "net/url"
+	q := make(url.Values)
+	q.Set("name", "")
+	req := httptest.NewRequest(http.MethodGet, "/catalog?"+q.Encode(), nil)
+	rec := httptest.NewRecorder()
+	ctx := e.NewContext(req, rec)
+	ctx.SetPath("/:placeID/catalog")
+	ctx.SetParamNames("placeID")
+	ctx.SetParamValues("1")
+
+	mockService := new(MockService)
+	h := NewHandler(mockService)
+	placeID := 1
+	name := ""
+
+	// Setup Env
+	t.Setenv("BASE_URL", "localhost:8080")
+
+	internalServerError := errors.Wrap(ErrInternalServerError, "test")
+	expectedResponse := util.APIResponse{
+		Status:  http.StatusInternalServerError,
+		Message: "internal server error",
+	}
+
+	expectedResponseJson, _ := json.Marshal(expectedResponse)
+
+	// Excpectation
+	var listItem ListItem
+	mockService.On("GetListItem", placeID, name).Return(&listItem, internalServerError)
+	
+	// Tes
+	assert.NoError(t, h.GetListItem(ctx))
+	assert.Equal(t, http.StatusInternalServerError, rec.Code)
 	assert.Equal(t, string(expectedResponseJson), strings.TrimSuffix(rec.Body.String(), "\n"))
 }
