9beebcaddd08b47c71f365a6dc08759bcdc62379
gerrad.natanael <gerrad.natanael@ui.ac.id>
2022-03-08 20:31:45 +0700

    [GREEN] add implementation to unwrap errors

--- util/errors.go
+++ util/errors.go
@@ -1,1 +1,10 @@
 package util
+
+import "strings"
+
+func ErrorUnwrap(err error) ([]string, string) {
+	errString := strings.Split(err.Error(), ":")
+	errList, errMessage := errString[0], errString[1]
+
+	return strings.Split(errList, ","), strings.TrimSpace(errMessage)
+}
