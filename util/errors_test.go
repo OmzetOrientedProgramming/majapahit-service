ee44eb652ad5c2fc5782c902663585319e2c8760
gerrad.natanael <gerrad.natanael@ui.ac.id>
2022-03-08 19:51:49 +0700

    [RED] add test for unwraping errors

---
+++ util/errors_test.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestErrorUnwrap(t *testing.T) {
+	expectation := []string{
+		"error 1", "error 2",
+	}
+	errMessageExpectation := errors.New("test error")
+
+	err := errors.Wrap(errMessageExpectation, strings.Join(expectation, ","))
+
+	errorList, message := ErrorUnwrap(err)
+
+	assert.Equal(t, expectation, errorList)
+	assert.Equal(t, errMessageExpectation.Error(), message)
+}
