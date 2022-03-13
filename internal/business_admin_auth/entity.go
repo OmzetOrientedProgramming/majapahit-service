19771e3b402b02552ef37030074659c3e508ead5
danan.maulidan <danan.maulidan@ui.ac.id>
2022-03-13 10:53:43 +0700

    Updating skeleton

--- internal/business_admin_auth/entity.go
+++ internal/business_admin_auth/entity.go
@@ -1,69 +1,43 @@
 package businessadminauth
 
-import (
-	"time"
-)
-
-type ListUser struct {
-	BusinessAdmins []BusinessAdmin `json:"business_admins"`
-	Count          int             `json:"count"`
-}
-
-type ListPlace struct {
-	Places []Place `json:"places"`
-	Count  int     `json:"count"`
-}
-
-type BusinessAdmin struct {
-	ID          int       `json:"id"`
-	PhoneNumber string    `json:"phone_number"`
-	Email       string    `json:"email"`
-	PlaceID     int       `json:"place_id"`
-	IsActive    bool      `json:"is_active"`
-	BankAccount int       `json:"bank_account"`
-	Balance     int       `json:"balance"`
-	JoinedAt    time.Time `json:"date_joined"`
-	UpdatedAt   time.Time `json:"date_updated"`
-	Password    string    `json:"password"`
-}
-
-type Place struct {
-	ID             int       `json:"id"`
-	Name           string    `json:"name"`
-	Address        string    `json:"address"`
-	Capacity       int       `json:"capacity"`
-	Description    string    `json:"description"`
-	AdminID        int       `json:"admin_id"`
-	Interval       string    `json:"interval"`
-	OpenHour       string    `json:"open_hour"`
-	CloseHour      string    `json:"close_hour"`
-	Image          string    `json:"image"`
-	MinHourBooking int       `json:"min_hour_booking"`
-	MaxHourBooking int       `json:"max_hour_booking"`
-	MinSlotBooking int       `json:"min_slot_booking"`
-	MaxSlotBooking int       `json:"max_slot_booking"`
-	Lat            float64   `json:"lat"`
-	Long           float64   `json:"long"`
-	CreatedAt      time.Time `json:"created_at"`
-	UpdatedAt      time.Time `json:"updated_at"`
+type User struct {
+	ID          int    `json:"id"`
+	PhoneNumber string `json:"phone_number"`
+	Name        string `json:"name"`
+	Email       string `json:"email"`
 }
 
 type RegisterBusinessAdminRequest struct {
-	AdminPhoneNumber    string  `json:"admin_phone_number"`
-	AdminEmail          string  `json:"admin_email"`
-	AdminBankAccount    string  `json:"admin_bank_account"`
-	PlaceName           string  `json:"place_name"`
-	PlaceAddress        string  `json:"place_address"`
-	PlaceCapacity       int     `json:"place_capacity"`
-	PlaceDescription    string  `json:"place_description"`
-	PlaceInterval       string  `json:"place_interval"`
-	PlaceOpenHour       string  `json:"place_open_hour"`
-	PlaceCloseHour      string  `json:"place_close_hour"`
-	PlaceImage          string  `json:"place_image"`
-	PlaceMinHourBooking int     `json:"place_min_hour_booking"`
-	PlaceMaxHourBooking int     `json:"place_max_hour_booking"`
-	PlaceMinSlotBooking int     `json:"place_min_slot_booking"`
-	PlaceMaxSlotBooking int     `json:"place_max_slot_booking"`
-	PlaceLat            float64 `json:"place_lat"`
-	PlaceLong           float64 `json:"place_long"`
+	AdminPhoneNumber        string  `json:"admin_phone_number"`
+	AdminEmail              string  `json:"admin_email"`
+	AdminBankAccount        string  `json:"admin_bank_account"`
+	AdminName               string  `json:"admin_name"`
+	AdminBankAccountName    string  `json:"admin_bank_account_name"`
+	PlaceName               string  `json:"place_name"`
+	PlaceAddress            string  `json:"place_address"`
+	PlaceCapacity           int     `json:"place_capacity"`
+	PlaceDescription        string  `json:"place_description"`
+	PlaceInterval           int     `json:"place_interval"`
+	PlaceOpenHour           string  `json:"place_open_hour"`
+	PlaceCloseHour          string  `json:"place_close_hour"`
+	PlaceImage              string  `json:"place_image"`
+	PlaceMinIntervalBooking int     `json:"place_min_interval_booking"`
+	PlaceMaxIntervalBooking int     `json:"place_max_interval_booking"`
+	PlaceMinSlotBooking     int     `json:"place_min_slot_booking"`
+	PlaceMaxSlotBooking     int     `json:"place_max_slot_booking"`
+	PlaceLat                float64 `json:"place_lat"`
+	PlaceLong               float64 `json:"place_long"`
+}
+
+type RegisterBusinessAdminResponse struct {
+	Status  int             `json:"status"`
+	Message string          `json:"message"`
+	Data    LoginCredential `json:"data,omitempty"`
+	Errors  []string        `json:"errors,omitempty"`
+}
+
+type LoginCredential struct {
+	PlaceName string `json:"place_name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
 }
--- internal/business_admin_auth/errors.go
+++ internal/business_admin_auth/errors.go
@@ -3,5 +3,9 @@ package businessadminauth
 import "github.com/pkg/errors"
 
 var (
-	ErrInternalServerError = errors.New("internal server error")
+	ErrInternalServerError     = errors.New("internal server error")
+	ErrInputValidationError    = errors.New("input validation error")
+	ErrUserNotFound            = errors.New("user not found")
+	ErrPhoneNumberAlreadyExist = errors.New("phone_number is already taken")
+	ErrNameIsTooShort          = errors.New("name is too short")
 )
