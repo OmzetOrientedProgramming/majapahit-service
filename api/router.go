73b10035059f42aa3bf9a55ecc729dd371e00fa0
Teofanus Gary Setiawan <garyteofanus@gmail.com>
2022-03-14 15:27:48 +0700

    PBI-1 Customer Registration

--- api/router.go
+++ api/router.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"github.com/labstack/echo/v4"
+	"gitlab.cs.ui.ac.id/ppl-fasilkom-ui/2022/Kelas-B/OOP/majapahit-service/internal/auth"
 	"gitlab.cs.ui.ac.id/ppl-fasilkom-ui/2022/Kelas-B/OOP/majapahit-service/internal/checkup"
 	"gitlab.cs.ui.ac.id/ppl-fasilkom-ui/2022/Kelas-B/OOP/majapahit-service/internal/item"
 	"gitlab.cs.ui.ac.id/ppl-fasilkom-ui/2022/Kelas-B/OOP/majapahit-service/internal/place"
@@ -13,13 +14,15 @@ type Routes struct {
 	checkUPHandler *checkup.Handler
 	catalogHandler *item.Handler
 	placeHandler   *place.Handler
+	authHandler    *auth.Handler
 }
 
 // NewRoutes for creating Routes instance
-func NewRoutes(router *echo.Echo, checkUpHandler *checkup.Handler, placeHandler *place.Handler) *Routes {
+func NewRoutes(router *echo.Echo, checkUpHandler *checkup.Handler, catalogHandler *item.Handler, placeHandler *place.Handler, authHandler *auth.Handler) *Routes {
 	return &Routes{
 		Router:         router,
 		checkUPHandler: checkUpHandler,
+		authHandler:    authHandler,
 		catalogHandler: catalogHandler,
 		placeHandler:   placeHandler,
 	}
@@ -32,14 +35,24 @@ func (r *Routes) Init() {
 
 	// V1
 	v1 := r.Router.Group("/api/v1")
+	{
+		// Place module
+		placeRoutes := v1.Group("/place")
+		placeRoutes.GET("", r.placeHandler.GetPlacesListWithPagination)
+		placeRoutes.GET("/:placeID", r.placeHandler.GetDetail)
+		{
+			// Catalog module
+			catalogRoutes := placeRoutes.Group("/:placeID/catalog")
+			catalogRoutes.GET("", r.catalogHandler.GetListItemWithPagination)
+			catalogRoutes.GET("/:itemID", r.catalogHandler.GetItemByID)
+		}
 
-	// Place module
-	place := v1.Group("/place")
-	place.GET("", r.placeHandler.GetPlacesListWithPagination)
-	place.GET("/:placeID", r.placeHandler.GetDetail)
-
-	// Catalog module
-	catalog := place.Group("/:placeID/catalog")
-	catalog.GET("", r.catalogHandler.GetListItemWithPagination)
-	catalog.GET("/:itemID", r.catalogHandler.GetItemByID)
+		// Auth module
+		authRoutes := v1.Group("/auth")
+		{
+			authRoutes.POST("/check-phone-number", r.authHandler.CheckPhoneNumber)
+			authRoutes.POST("/verifyPhoneNumber", r.authHandler.VerifyOTP)
+			authRoutes.POST("/register", r.authHandler.Register)
+		}
+	}
 }
