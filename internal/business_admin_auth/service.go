58ed47581982c1991e78e9f8bdc91dea8032fc75
danan.maulidan <danan.maulidan@ui.ac.id>
2022-03-14 16:37:06 +0700

    [GREEN] Adding implementation for service

--- internal/business_admin_auth/service.go
+++ internal/business_admin_auth/service.go
@@ -1,5 +1,11 @@
 package businessadminauth
 
+import (
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
 func NewService(repo Repo) Service {
 	return &service{
 		repo: repo,
@@ -7,13 +13,82 @@ func NewService(repo Repo) Service {
 }
 
 type Service interface {
-	RegisterBusinessAdmin() (bool, error)
+	RegisterBusinessAdmin(request RegisterBusinessAdminRequest) (*LoginCredential, error)
 }
 
 type service struct {
 	repo Repo
 }
 
-func (s service) RegisterBusinessAdmin() (bool, error) {
-	panic("implement me")
+func (s service) RegisterBusinessAdmin(request RegisterBusinessAdminRequest) (*LoginCredential, error) {
+	var (
+		credentials LoginCredential
+		errorList   []string
+	)
+
+	// Check required overall
+	errorList = s.repo.CheckRequiredFields(request, errorList)
+	if len(errorList) != 0 {
+		return nil, errors.Wrap(ErrInputValidationError, strings.Join(errorList, ","))
+	}
+
+	// Check Fields for User
+	err := s.repo.CheckUserFields(request)
+	if err != nil {
+		return nil, err
+	}
+
+	// Generating Password for User
+	password := s.repo.GeneratePassword()
+	if password == "" {
+		return nil, errors.Wrap(ErrInternalServerError, "error while generating password")
+	}
+
+	// Creating User
+	var status = 1 // business_owners
+	err = s.repo.CreateUser(request.AdminPhoneNumber, request.AdminName, request.AdminEmail, password, status)
+	if err != nil {
+		return nil, err
+	}
+
+	// Retrieving User ID
+	userId, err := s.repo.RetrieveUserId(request.AdminPhoneNumber)
+	if err != nil {
+		return nil, err
+	}
+
+	// Check Fields for BusinessAdmin
+	err = s.repo.CheckBusinessAdminFields(request)
+	if err != nil {
+		return nil, err
+	}
+
+	// Creating new BusinessAdmin
+	var balance float32 = 0.0
+	err = s.repo.CreateBusinessAdmin(userId, request.AdminBankAccount, request.AdminBankAccountName, balance)
+	if err != nil {
+		return nil, err
+	}
+
+	// Check Fields for Place
+	err = s.repo.CheckPlaceFields(request)
+	if err != nil {
+		return nil, err
+	}
+
+	// Creating new Place
+	err = s.repo.CreatePlace(request.PlaceName, request.PlaceAddress, request.PlaceCapacity,
+		request.PlaceDescription, userId, request.PlaceInterval, request.PlaceOpenHour, request.PlaceCloseHour,
+		request.PlaceImage, request.PlaceMinIntervalBooking, request.PlaceMaxIntervalBooking, request.PlaceMinSlotBooking,
+		request.PlaceMaxSlotBooking, request.PlaceLat, request.PlaceLong)
+	if err != nil {
+		return nil, err
+	}
+
+	// Inserting Login Credentials
+	credentials.PlaceName = request.PlaceName
+	credentials.Email = request.AdminEmail
+	credentials.Password = password
+
+	return &credentials, nil
 }
