4ad1417fd081dd6e7e21ff99a0904b76ff5a8693
gerrad.natanael <gerrad.natanael@ui.ac.id>
2022-03-21 20:30:37 +0700

    [REFACTOR] update customer booking entity

--- internal/customer_booking/entity.go
+++ internal/customer_booking/entity.go
@@ -3,7 +3,6 @@ package customerbooking
 // CustomerBooking contains information that are needed by Business Admin
 type CustomerBooking struct {
 	ID           	int    	`json:"id"`
-	CustomerName	string 	`json:"name"`
 	Capacity     	int    	`json:"capacity"`
 	Date			string	`json:"date"`
 	StartTime    	string 	`json:"start_time" db:"start_time"`
@@ -12,14 +11,15 @@ type CustomerBooking struct {
 
 // List is a container for customer bookings
 type List struct {
-	CustomerBooking []CustomerBooking `json:"booking_customer"`
+	CustomerBookings []CustomerBooking `json:"booking_customer"`
 	TotalCount      int               `json:"total_count"`
 }
 
 // ListRequest consists of request data from client
 type ListRequest struct {
-	Limit int    `json:"limit"`
-	Page  int    `json:"page"`
-	State int    `json:"state"`
-	Path  string `json:"path"`
+	Limit 		int    	`json:"limit"`
+	Page  		int    	`json:"page"`
+	State 		int    	`json:"state"`
+	PlaceID 	int    	`json:"place_id"`
+	Path  		string 	`json:"path"`
 }
