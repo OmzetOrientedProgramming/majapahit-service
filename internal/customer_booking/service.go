73c34a6061ae79ea88106b14bdc5ef3f154c310b
gerrad.natanael <gerrad.natanael@ui.ac.id>
2022-03-23 19:35:09 +0700

    [GREEN] add implementation to get list customer booking in service

--- internal/customer_booking/service.go
+++ internal/customer_booking/service.go
@@ -1,5 +1,9 @@
 package customerbooking
 
+import (
+	"gitlab.cs.ui.ac.id/ppl-fasilkom-ui/2022/Kelas-B/OOP/majapahit-service/util"
+)
+
 // NewService for initialize service
 func NewService(repo Repo) Service {
 	return &service{
@@ -9,9 +13,19 @@ func NewService(repo Repo) Service {
 
 // Service will contain all the function that can be used by service
 type Service interface {
+	GetListCustomerBookingWithPagination(params ListRequest) (*List, *util.Pagination, error)
 }
 
 type service struct {
 	repo Repo
 }
 
+func (s service) GetListCustomerBookingWithPagination(params ListRequest) (*List, *util.Pagination, error) {
+	listCustomerBooking, err := s.repo.GetListCustomerBookingWithPagination(params)
+	if err != nil {
+		return nil, nil, err
+	}
+	pagination := util.GeneratePagination(listCustomerBooking.TotalCount, params.Limit, params.Page, params.Path)
+
+	return listCustomerBooking, &pagination, err
+}
