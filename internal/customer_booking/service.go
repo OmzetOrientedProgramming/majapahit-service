123502e70790358cb5487f9c3c5901d60f04ab0d
gerrad.natanael <gerrad.natanael@ui.ac.id>
2022-03-23 20:32:04 +0700

    [GREEN] add implementation to handle null params and errors

--- internal/customer_booking/service.go
+++ internal/customer_booking/service.go
@@ -1,6 +1,9 @@
 package customerbooking
 
 import (
+	"strings"
+
+	"github.com/pkg/errors"
 	"gitlab.cs.ui.ac.id/ppl-fasilkom-ui/2022/Kelas-B/OOP/majapahit-service/util"
 )
 
@@ -21,6 +24,32 @@ type service struct {
 }
 
 func (s service) GetListCustomerBookingWithPagination(params ListRequest) (*List, *util.Pagination, error) {
+	var errorList []string;
+
+	if params.State == 0{
+		params.State = 1
+	}
+
+	if params.Page == 0 {
+		params.Page = util.DefaultPage
+	}
+
+	if params.Limit == 0 {
+		params.Limit = util.DefaultLimit
+	}
+
+	if params.Limit > util.MaxLimit {
+		errorList = append(errorList, "limit should be 1 - 100")
+	}
+
+	if params.Path == "" {
+		errorList = append(errorList, "path is required for pagination")
+	}
+
+	if len(errorList) > 0 {
+		return nil, nil, errors.Wrap(ErrInputValidationError, strings.Join(errorList, ","))
+	}
+	
 	listCustomerBooking, err := s.repo.GetListCustomerBookingWithPagination(params)
 	if err != nil {
 		return nil, nil, err
