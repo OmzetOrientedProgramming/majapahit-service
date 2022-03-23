01dd7a01a574deb1339fda324abadc1c38a7a3c5
gerrad.natanael <gerrad.natanael@ui.ac.id>
2022-03-23 15:12:20 +0700

    [RED] add tests to handle internal server error

--- internal/customer_booking/repo_test.go
+++ internal/customer_booking/repo_test.go
@@ -1,11 +1,13 @@
 package customerbooking
 
 import (
+	"database/sql"
 	"regexp"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -76,4 +78,68 @@ func TestRepo_GetListCustomerBookingWwithPaginationSuccess(t *testing.T) {
 	assert.Equal(t, listCustomerBookingExpected, listItemResult)
 	assert.NotNil(t, listItemResult)
 	assert.NoError(t, err)
+}
+
+func TestRepo_GetListCustomerBookingWithPaginationError(t *testing.T) {
+	params := ListRequest{
+		Limit:   10,
+		Page:    1,
+		State:   1,
+		PlaceID: 1,
+	}
+
+	// Mock DB
+	mockDB, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer mockDB.Close()
+
+	// Expectation
+	sqlxDB := sqlx.NewDb(mockDB, "sqlmock")
+	repoMock := NewRepo(sqlxDB)
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT b.id, u.name, b.capacity, b.date, b.start_time, b.end_time FROM bookings b, users u WHERE b.place_id = $1 AND u.id = b.user_id AND b.status = $2 LIMIT $3 OFFSET $4")).
+		WithArgs(params.PlaceID, params.State, params.Limit, (params.Page-1)*params.Limit).
+		WillReturnError(sql.ErrTxDone)
+
+	// Test
+	listItemResult, err := repoMock.GetListCustomerBookingWithPagination(params)
+	assert.Nil(t, listItemResult)
+	assert.Equal(t, ErrInternalServerError, errors.Cause(err))
+
+}
+
+func TestRepo_GetListCustomerBookingWithPaginationCountError(t *testing.T) {
+	params := ListRequest{
+		Limit:   10,
+		Page:    1,
+		State:   1,
+		PlaceID: 1,
+	}
+
+	// Mock DB
+	mockDB, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer mockDB.Close()
+
+	// Expectation
+	sqlxDB := sqlx.NewDb(mockDB, "sqlmock")
+	repoMock := NewRepo(sqlxDB)
+	rows := mock.
+		NewRows([]string{"id", "name", "capacity", "date", "start_time", "end_time"}).
+		AddRow("1", "test name", 1, "test date", "test start time", "test end time")
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT b.id, u.name, b.capacity, b.date, b.start_time, b.end_time FROM bookings b, users u WHERE b.place_id = $1 AND u.id = b.user_id AND b.status = $2 LIMIT $3 OFFSET $4")).
+		WithArgs(params.PlaceID, params.State, params.Limit, (params.Page-1)*params.Limit).
+		WillReturnRows(rows)
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT COUNT(id) FROM bookings WHERE place_id = $1")).
+		WithArgs(params.PlaceID).
+		WillReturnError(sql.ErrConnDone)
+
+	// Test
+	listItemResult, err := repoMock.GetListCustomerBookingWithPagination(params)
+	assert.Nil(t, listItemResult)
+	assert.Equal(t, ErrInternalServerError, errors.Cause(err))
+
 }
