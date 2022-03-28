f38801dbff0208c6de57c6219e47f19f7ac01aae
gerrad.natanael <gerrad.natanael@ui.ac.id>
2022-03-28 16:01:38 +0700

    [RED] add tests for params state, limit, and page

--- internal/customer_booking/handler_test.go
+++ internal/customer_booking/handler_test.go
@@ -109,3 +109,161 @@ func TestHandler_GetListCustomerBookingWithPaginationSuccess(t *testing.T) {
 		assert.Equal(t, string(expectedResponseJSON), strings.TrimSuffix(rec.Body.String(), "\n"))
 	}
 }
+
+func TestHandler_GetListCustromerBookingWithPaginationPlaceIDError(t *testing.T) {
+	// Setup echo
+	e := echo.New()
+
+	// import "net/url"
+	q := make(url.Values)
+	q.Set("state", "1")
+	q.Set("limit", "10")
+	q.Set("page", "1")
+	req := httptest.NewRequest(http.MethodGet, "/booking?"+q.Encode(), nil)
+	rec := httptest.NewRecorder()
+	ctx := e.NewContext(req, rec)
+	ctx.SetPath("/:placeID/booking")
+	ctx.SetParamNames("placeID")
+	ctx.SetParamValues("test")
+
+	mockService := new(MockService)
+	h := NewHandler(mockService)
+
+	// Setup Env
+	t.Setenv("BASE_URL", "localhost:8080")
+
+	expectedResponse := util.APIResponse{
+		Status:  http.StatusBadRequest,
+		Message: "input validation error",
+		Errors: []string{
+			"incorrect place id",
+		},
+	}
+
+	expectedResponseJSON, _ := json.Marshal(expectedResponse)
+
+	// Tes
+	assert.NoError(t, h.GetListCustomerBookingWithPagination(ctx))
+	assert.Equal(t, http.StatusBadRequest, rec.Code)
+	assert.Equal(t, string(expectedResponseJSON), strings.TrimSuffix(rec.Body.String(), "\n"))
+}
+
+func TestHandler_GetListItemWithPaginationStateAndLimitAndPageAreNotInt(t *testing.T) {
+	// Setup echo
+	e := echo.New()
+
+	// import "net/url"
+	q := make(url.Values)
+	q.Set("state", "asd")
+	q.Set("limit", "asd")
+	q.Set("page", "asd")
+	req := httptest.NewRequest(http.MethodGet, "/booking?"+q.Encode(), nil)
+	rec := httptest.NewRecorder()
+	ctx := e.NewContext(req, rec)
+	ctx.SetPath("/:placeID/booking")
+	ctx.SetParamNames("placeID")
+	ctx.SetParamValues("1")
+
+	mockService := new(MockService)
+	h := NewHandler(mockService)
+
+	// Setup Env
+	t.Setenv("BASE_URL", "localhost:8080")
+
+	expectedResponse := util.APIResponse{
+		Status:  http.StatusBadRequest,
+		Message: "input validation error",
+		Errors: []string{
+			"state should be positive integer",
+			"limit should be positive integer",
+			"page should be positive integer",
+		},
+	}
+
+	expectedResponseJSON, _ := json.Marshal(expectedResponse)
+
+	// Tes
+	assert.NoError(t, h.GetListCustomerBookingWithPagination(ctx))
+	assert.Equal(t, http.StatusBadRequest, rec.Code)
+	assert.Equal(t, string(expectedResponseJSON), strings.TrimSuffix(rec.Body.String(), "\n"))
+}
+
+func TestHandler_GetListCustomerBookingWithPaginationWithStateLimitPageAreEmpty(t *testing.T) {
+	// Setup echo
+	e := echo.New()
+
+	// import "net/url"
+	q := make(url.Values)
+	req := httptest.NewRequest(http.MethodGet, "/booking?"+q.Encode(), nil)
+	rec := httptest.NewRecorder()
+	ctx := e.NewContext(req, rec)
+	ctx.SetPath("/:placeID/booking")
+	ctx.SetParamNames("placeID")
+	ctx.SetParamValues("1")
+
+	mockService := new(MockService)
+	h := NewHandler(mockService)
+
+	params := ListRequest{
+		Limit:   0,
+		Page:    0,
+		Path:    "/api/v1/business-admin/1/booking",
+		State: 0,
+		PlaceID: 1,
+	}
+
+	// Setup Env
+	t.Setenv("BASE_URL", "localhost:8080")
+
+	listCustomerBooking := List{
+		CustomerBookings: []CustomerBooking{
+			{
+				ID:          1,
+				CustomerName: "test name 1",
+				Capacity:     10,
+				Date: "test date 1",
+				StartTime: "test start time 1",
+				EndTime: "test end time 1",
+			},
+			{
+				ID:          2,
+				CustomerName: "test name 2",
+				Capacity:     10,
+				Date: "test date 2",
+				StartTime: "test start time 2",
+				EndTime: "test end time 2",
+			},
+		},
+		TotalCount: 10,
+	}
+
+	pagination := util.Pagination{
+		Limit:       10,
+		Page:        1,
+		FirstURL:    fmt.Sprintf("%s/api/v1/business-admin/1/booking?state=1&limit=10&page=1", os.Getenv("BASE_URL")),
+		LastURL:     fmt.Sprintf("%s/api/v1/business-admin/1/booking?state=1&limit=10&page=1", os.Getenv("BASE_URL")),
+		NextURL:     fmt.Sprintf("%s/api/v1/business-admin/1/booking?state=1&limit=10&page=1", os.Getenv("BASE_URL")),
+		PreviousURL: fmt.Sprintf("%s/api/v1/business-admin/1/booking?state=1&limit=10&page=1", os.Getenv("BASE_URL")),
+		TotalPage:   1,
+	}
+
+	expectedResponse := util.APIResponse{
+		Status:  http.StatusOK,
+		Message: "success",
+		Data: map[string]interface{}{
+			"bookings":      listCustomerBooking.CustomerBookings,
+			"pagination": pagination,
+		},
+	}
+
+	expectedResponseJSON, _ := json.Marshal(expectedResponse)
+
+	// Excpectation
+	mockService.On("GetListCustomerBookingWithPagination", params).Return(&listCustomerBooking, pagination, nil)
+
+	// Tes
+	if assert.NoError(t, h.GetListCustomerBookingWithPagination(ctx)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, string(expectedResponseJSON), strings.TrimSuffix(rec.Body.String(), "\n"))
+	}
+}
