68a65d421cfbccba83ca66e788f6ed9065c106dc
Muhammad Haikal Ubaidillah Susanto <haikalsusanto21@gmail.com>
2022-04-01 03:35:01 +0700

    [GREEN] passed test handler GetDetailSuccess

--- internal/booking/handler.go
+++ internal/booking/handler.go
@@ -1,6 +1,12 @@
 package booking
 
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+	"gitlab.cs.ui.ac.id/ppl-fasilkom-ui/2022/Kelas-B/OOP/majapahit-service/util"
+)
 
 // Handler struct for place package
 type Handler struct {
@@ -16,5 +22,20 @@ func NewHandler(service Service) *Handler {
 
 // GetDetail will retrieve information related to a booking
 func (h *Handler) GetDetail(c echo.Context) error {
-	panic("Not yet implemented!")
+	bookingIDString := c.Param("bookingID")
+	bookingID, err := strconv.Atoi(bookingIDString)
+	if err != nil {
+		panic("error atoi")
+	}
+
+	bookingDetail, err := h.service.GetDetail(bookingID)
+	if err != nil {
+		panic("error get detail service")
+	}
+
+	return c.JSON(http.StatusOK, util.APIResponse{
+		Status:  http.StatusOK,
+		Message: "success",
+		Data:    bookingDetail,
+	})
 }
